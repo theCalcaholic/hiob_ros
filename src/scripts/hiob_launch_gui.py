c5579cbdfffcaa2a2f66efba7a6fcf11b1bddfdd
Tobias Knöppler (3knoeppl) <3knoeppl@informatik.uni-hamburg.de>
2017-11-16 19:12:54 +0100

    Push hiob to latest version. Fix ros wrappers.

--- src/scripts/hiob_launch_gui.py
+++ src/scripts/hiob_launch_gui.py
@@ -1,10 +1,10 @@
 #!/data/3knoeppl/bsc_thesis/venvs/hiob/bin/python
-# PYTHONPATH=../hiob python hy1.py
+# PYTHONPATH=.. python hy1.py
 
 import os
 
-import hiob_gui
+from hiob import hiob_gui
 
 if __name__ == '__main__':
-    os.chdir("../hiob")
+    os.chdir("./hiob")
     hiob_gui.main()
