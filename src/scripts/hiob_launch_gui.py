7c90594ee804068d38bf0f2c45974ec64661fcfb
Tobias Knöppler (3knoeppl) <3knoeppl@informatik.uni-hamburg.de>
2017-11-20 17:13:57 +0100

    Fix missing node initialization in gui launcher.

--- src/scripts/hiob_launch_gui.py
+++ src/scripts/hiob_launch_gui.py
@@ -2,9 +2,11 @@
 # PYTHONPATH=.. python hy1.py
 
 import os
+import rospy
 
 from hiob import hiob_gui
 
 if __name__ == '__main__':
+    rospy.init_node("hiob", anonymous=True)
     os.chdir(os.path.join(os.path.dirname(__file__), "hiob"))
     hiob_gui.main()
