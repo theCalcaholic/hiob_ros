9090c1b9d3131afa457d7dd492a60968f326ccbd
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2018-03-13 20:54:16 +0100

    Remove local venv mechanism due to broken relocate functionality in virtualenv.

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-VENV="./venv"
 
+local VENV=""
+
+
+if [ ! -z $HIOB_VENV ]; then
+  local VENV="$HIOB_VENV"
+fi
 
 cd "$(dirname "$0")"
-source "$VENV/bin/activate"
+source "$HIOB_VENV/bin/activate"
 
 echo `pwd`
 
-if [ "$1" == "--nogui" ] ; then
+if [ "$1" == "--no-gui" ] ; then
   python ./ros_launcher_cli.py
 else
   python ./ros_launcher_gui.py
