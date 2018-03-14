1ad1997a5d359cf1594294b5e9f885b1a5ab2b14
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2018-03-14 07:25:19 +0100

    Combine gui and cli launcher scripts into one. Adjust paths when run from devel (without 'catkin_make install').

---
+++ scripts/ros_launcher.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import rospy
+import rospkg
+
+
+if __name__ == '__main__':
+    if "--ros-subscribe" not in sys.argv:
+        raise Exception("hiob_ros requires the argument '--ros-subscribe <ros node>'. Exiting...")
+    os.sys.path.append(os.path.dirname(__file__))
+
+    rp = rospkg.RosPack()
+    config_path = os.path.join(rp.get_path('hiob_ros'), 'config')
+    my_path = os.path.realpath(os.path.dirname(__file__))
+
+    if os.path.isdir(os.path.join(my_path, "..", "..", "..", "src")):
+        print("Devel mode detected. Adjusting paths...")
+        os.chdir(os.path.join(my_path, '..', 'hiob'))
+        config_path = 'config'
+        if "-e" not in sys.argv:
+            sys.argv.extend(["-e", os.path.join(config_path, 'environment.yaml')])
+    else:
+        if "-e" not in sys.argv:
+            sys.argv.extend(["-e", os.path.join(config_path, 'environment_ros.yaml')])
+    if "-t" not in sys.argv:
+        sys.argv.extend(['-t', os.path.join(config_path, 'tracker_ros.yaml')])
+
+    rospy.init_node("hiob", anonymous=True)
+
+    if "--no-gui" in sys.argv:
+        from hiob import hiob_cli as hiob
+        sys.argv.remove('--no-gui')
+    else:
+        from hiob import hiob_gui as hiob
+
+    hiob.main()
